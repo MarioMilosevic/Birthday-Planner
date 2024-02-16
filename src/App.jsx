452213e73f060a318b51b0276be0b0b4d82f5163
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-02-16 19:58:52 +0100

    Render tours info

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-// import { useState } from 'react'
-// import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [tours, setTours] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://course-api.com/react-tours-project"
+      );
+      if (!response.ok) {
+        throw new Error("Network reponse was not ok");
+      }
+      const data = await response.json();
+      console.log(data);
+      setTours(data);
+    } catch (error) {
+      console.error("Error fetching data", error);
+    }
+  };
+
+  fetchData();
 
   return (
     <>
-      
+      {tours.map((tour) => (
+        <div key={tour.id}>
+          <img src={tour.image} alt="" />
+          <h2>{tour.name}</h2>
+          <p>{tour.info}</p>
+          <p>{tour.price}</p>
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
