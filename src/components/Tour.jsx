f6b964bc7e37de72385ff319328cfa58b1f9841f
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-02-17 16:35:42 +0100

    Add padding and font weight to price text

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,14 +1,14 @@
 const Tour = (props) => {
   return (
-    <div key={props.id} className="border-black bg-gray-200 p-4">
+    <div key={props.id} className="p-4">
       <img
         src={props.image}
         alt={props.image}
         className="w-96 h-60 items-center object-cover rounded-xl"
       />
       <h2 className="py-4 text-xl font-semibold">{props.name}</h2>
-      <p className="">{props.info}</p>
-      <p>For only {props.price}$</p>
+      <p className="pb-4">{props.info}</p>
+      <p className="text-2xl font-bold">For only {props.price}$</p>
     </div>
   );
 };
