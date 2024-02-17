8392daf04fd0500e4183c96643fe3ee1d6361c0d
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-02-17 14:25:53 +0100

    Add loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [tours, setTours] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch(
-        "https://course-api.com/react-tours-project"
-      );
-      if (!response.ok) {
-        throw new Error("Network reponse was not ok");
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(
+          "https://course-api.com/react-tours-project"
+        );
+        if (!response.ok) {
+          throw new Error("Network reponse was not ok");
+        }
+        const data = await response.json();
+        console.log(data);
+        setTours(data);
+      } catch (error) {
+        console.error("Error fetching data", error);
+      } finally {
+        setLoading(false);
       }
-      const data = await response.json();
-      console.log(data);
-      setTours(data);
-    } catch (error) {
-      console.error("Error fetching data", error);
-    }
-  };
+    };
+    fetchData();
+  }, []);
 
-  fetchData();
+  if (loading) return <h1>Loading...</h1>;
 
   return (
     <>
