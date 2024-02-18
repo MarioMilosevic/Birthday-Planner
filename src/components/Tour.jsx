df2935653654d21c319c762f6b74b2e2c0d83146
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-02-18 17:48:36 +0100

    Change div into article, add padding to img and information text

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,20 +1,17 @@
 const Tour = (props) => {
   return (
-    <div
-      key={props.id}
-      className=" gap-20 p-2 w-[560px]"
-    >
+    <article key={props.id} className=" gap-20 p-2 w-[560px]">
       <img
-        className="h-[550px] w-full object-cover rounded-xl"
+        className="h-[550px] w-full object-cover rounded-2xl p-2"
         src={props.image}
         alt={props.image}
-        />
-        <div>
-      <h2 className="py-4 text-xl font-semibold">{props.name}</h2>
-      <p className="pb-4 ">{props.info}</p>
-      <p className="text-2xl font-bold">For only {props.price}$</p>
-        </div>
-    </div>
+      />
+      <div>
+        <h2 className="py-4 px-2 text-xl font-semibold">{props.name}</h2>
+        <p className="pb-4 px-2">{props.info}</p>
+        <p className="text-2xl font-bold">For only {props.price}$</p>
+      </div>
+    </article>
   );
 };
 
