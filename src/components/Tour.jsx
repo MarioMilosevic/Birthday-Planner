902617eb82f51042ad07e38551809d6648dab49e
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-02-28 15:38:44 +0100

    Refactor

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,15 +1,15 @@
-const Tour = (props) => {
+const Tour = ({ id, image, price, info }) => {
   return (
-    <article key={props.id} className=" gap-20 p-2 w-[560px]">
+    <article key={id} className=" gap-20 p-2 w-[560px]">
       <img
         className="h-[550px] w-full object-cover rounded-2xl p-2"
-        src={props.image}
-        alt={props.image}
+        src={image}
+        alt={image}
       />
       <div>
-        <h2 className="py-4 px-2 text-xl font-semibold">{props.name}</h2>
-        <p className="pb-4 px-2">{props.info}</p>
-        <p className="text-2xl font-bold">For only {props.price}$</p>
+        <h2 className="py-4 px-2 text-xl font-semibold">{name}</h2>
+        <p className="pb-4 px-2">{info}</p>
+        <p className="text-2xl font-bold">For only {price}$</p>
       </div>
     </article>
   );
